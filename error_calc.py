03810420ead96aac48899e73c92f0bb0cc28d0dd
lawrence_muema_m <40681138+lawrencemuema@users.noreply.github.com>
2023-09-01 14:14:36 +0300

    Update error_calc.py

--- error_calc.py
+++ error_calc.py
@@ -8,10 +8,7 @@ def multiply(x, y):
     return x * y
 
 def divide(x, y):
-    if y == 0:
-        print("Cannot divide by zero!")
-    else:
-        return x / y
+    return x / y
 
 print("Simple Calculator")
 print("1. Add")
@@ -21,21 +18,18 @@ print("4. Divide")
 
 choice = input("Select operation (1/2/3/4): ")
 
-if choice not in ["1", "2", "3", "4"]:
-    print("Invalid input")
-else:
-    num1 = float(input("Enter first number: "))
-    num2 = float(input("Enter second number: "))
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
 
-    if choice == "1":
-        result = add(num1, num2)
-        print("Result: " + result)
-    elif choice == "2":
-        result = subtract(num1, num2)
-        print("Result: " + result)
-    elif choice == "3":
-        result = multiply(num1, num2)
-        print("Result: " + result)
-    elif choice == "4":
-        result = divide(num1, num2)
-        print("Result: " + result)
+if choice == "1":
+    result = add(num1, num2)
+    print("Result: " + result)
+elif choice == "2":
+    result = subtract(num1, num2)
+    print("Result: " + result)
+elif choice == "3":
+    result = multiply(num1, num2)
+    print("Result: " + result)
+elif choice == "4":
+    result = divide(num1, num2)
+    print("Result: " + result)
